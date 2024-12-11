203f292ba3d3f7e3b8bfc631f54bd85d26393a5a
Hans Mohrmann <hans@brightband.com>
2024-12-10 19:09:29 -0500

    formatting for linters

--- src/nnja/io.py
+++ src/nnja/io.py
@@ -2,9 +2,9 @@ import fsspec
 import json
 from typing import Literal
 
-Backend = Literal['pandas', 'polars', 'dask']
+Backend = Literal["pandas", "polars", "dask"]
 
 
 def read_json(json_uri: str) -> dict:
-    with fsspec.open(json_uri, mode='r') as f:
+    with fsspec.open(json_uri, mode="r") as f:
         return json.load(f)
