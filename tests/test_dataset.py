a646fcfed9a14aff5abc3648aa2dcdb097eada6f
Hans Mohrmann <hans@brightband.com>
2024-12-10 22:43:33 -0500

    adding unit tests for dataset.py

---
+++ tests/test_dataset.py
@@ -0,0 +1,31 @@
+from nnja.dataset import NNJADataset
+
+
+def test_dataset_initialization():
+    dataset = NNJADataset("sample_data/adpsfc_nc000001_dataset.json")
+    assert dataset.name == "WMOSYNOP_fixed"
+    assert (
+        dataset.description
+        == "Synoptic - fixed land (originating from WMO SYNOP bulletins)."
+    )
+    assert dataset.tags == ["surface", "fixed-station", "synoptic", "wmo"]
+    assert len(dataset.manifest) == 0
+    assert len(dataset.variables) == 3
+
+
+def test_variable_expansion():
+    dataset = NNJADataset("sample_data/amsu_dataset.json")
+    variables = dataset.variables
+    channels = [1, 2, 3, 4, 5]
+    expected_variables = ["lat", "lon", "time", "said", "fovn"]
+    expected_channel_vars = [f"brightness_temp_{channel:05.0f}" for channel in channels]
+    expected_variables = expected_variables + expected_channel_vars
+    assert set(variables) == set(expected_variables)
+    assert dataset.dimensions.keys() == {"channel"}
+
+
+def test_get_variable():
+    dataset = NNJADataset("sample_data/amsu_dataset.json")
+    variable = dataset["lat"]
+    assert variable.id == "lat"
+    assert variable.description == "Latitude of the observation."
