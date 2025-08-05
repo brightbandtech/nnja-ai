1a312a017561e5bb01642d8ae7211e19bd6efc74
Daniel Rothenberg <daniel@danielrothenberg.com>
2025-08-05 13:17:08 -0600

    refactor: remove old nnja package directory

--- src/nnja/__init__.py
+++
@@ -1,9 +0,0 @@
-from importlib.metadata import version
-
-__version__ = version("nnja")
-
-from nnja.catalog import DataCatalog
-from nnja.dataset import NNJADataset
-from nnja.variable import NNJAVariable
-
-__all__ = ["DataCatalog", "NNJADataset", "NNJAVariable"]
